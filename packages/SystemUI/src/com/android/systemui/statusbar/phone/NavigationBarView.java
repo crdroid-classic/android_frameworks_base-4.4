12da1c54bda13d87a19a58a7af148f52662cc40a
Nextbit <contributions@nextbit.com>
2014-11-17 15:13:29 -0800

    Remove Application Widget from LockPatternUtils
    The application widget option has been removed from Settings, and should
    instead be handled by the application granted with the
    SET_APPLICATION_WIDGET permission.
    
    This change:
    - removes the configuration value from the LockScreenSettings database; and
    - removes the producers and consumers of the value.
    
    Change-Id: Ia83e8d713d4455a9c898a8048a256afb7291af0b

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/NavigationBarView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/NavigationBarView.java
@@ -789,7 +789,7 @@ public class NavigationBarView extends LinearLayout implements BaseStatusBar.Nav
                 && mLockUtils.getCameraEnabled();
         // TODO(): Ideally we should integrate with DevicePolicyManager for application widget too.
         final boolean showApplicationWidget = showSearch &&
-                mApplicationWidgetPackageName != null && mLockUtils.getApplicationWidgetEnabled();
+                mApplicationWidgetPackageName != null;
         final boolean showNotifs = showSearch &&
             Settings.System.getInt(mContext.getContentResolver(),
                         Settings.System.LOCKSCREEN_NOTIFICATIONS, 1) == 1 &&
